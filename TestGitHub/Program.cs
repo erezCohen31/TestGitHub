5e332aceff09f9235a7566144bcbb8be66e75087
erez cohen <erezjbh@gmail.com>
2025-05-14 12:29:30 +0300

    tets pull

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("this is a repo");
 Console.WriteLine("this is a new branch");
+Console.WriteLine("I will test pull");
+
 
